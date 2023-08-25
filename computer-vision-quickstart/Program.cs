d6b7e4777deb3dadd6e287af82af30e5588919bf
Audrey Long <aulong@microsoft.com>
2023-08-25 10:58:34 -0400

    added mulit-prompt completions, still needs to be fixed, each prompt is treated independently

--- computer-vision-quickstart/Program.cs
+++ computer-vision-quickstart/Program.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using DotNetEnv;
 using System.Threading.Tasks;
+using Azure.AI.OpenAI;
 
 namespace ThreatModelGPT
 {
@@ -23,6 +24,7 @@ namespace ThreatModelGPT
             string computerVisionApiKey = Env.GetString("COMPUTER_VISION_API_KEY");
             string computerVisionApiEndpoint = Env.GetString("COMPUTER_VISION_API_ENDPOINT");
             string imageFilePath = Env.GetString("IMAGE_FILEPATH");
+  
 
             Console.WriteLine("Azure Cognitive Services Computer Vision - .NET quickstart example");
             ComputerVisionClient client = Authenticate(computerVisionApiEndpoint, computerVisionApiKey);
@@ -36,11 +38,11 @@ namespace ThreatModelGPT
             // Use OpenAI API to generate recommendations from the extracted text
             var recommendations = await GenerateOpenAIRecommendations(extractedText, openAiApiKey, openAiApiendpoint);
 
-            Console.WriteLine("Recommended Actions:");
+          /*  Console.WriteLine("Recommended Actions:");
             foreach (var recommendation in recommendations)
             {
                 Console.WriteLine(recommendation);
-            }
+            }*/
         }
 
         public static ComputerVisionClient Authenticate(string endpoint, string key)
@@ -99,49 +101,38 @@ namespace ThreatModelGPT
 
        public static async Task<List<string>> GenerateOpenAIRecommendations(string text, string apiKey, string apiEndpoint)
         {
+            string engine = "text-davinci-003";
             List<string> recommendations = new List<string>(); // Initialize the list
+            List<string> prompts = new(){
+                $"Prompt 1: You are a Microsoft security engineer doing threat model analysis to identity and mitigate risk. Given the following text:\n{text}\n please find the keywords relevant to a security engineer and print them out. \n",
+                $"Prompt 2: Enumerate the results from the previous prompt. \n"
 
-            string engine = "audreysmodel";
+            };
+
             OpenAIClient client = new OpenAIClient(new Uri(apiEndpoint), new AzureKeyCredential(apiKey));
 
-            string prompt = $" You are a Microsoft security engineer doing threat model analysis to identity and mitigate risk. Given the following text:\n{text}\n  Please provide the relevant keywords from the image:";
-            Console.Write($"Input: {prompt}\n");
+            foreach (string prompt in prompts)
+            {
+                Console.Write($"Input: {prompt}");
+                CompletionsOptions completionsOptions = new CompletionsOptions();
+                completionsOptions.Prompts.Add(prompt);
+                completionsOptions.MaxTokens = 500;
+                completionsOptions.Temperature = 0.2f;
 
-            Response<Completions> completionsResponse = await client.GetCompletionsAsync(engine, prompt);
+                Response<Completions> completionsResponse = client.GetCompletions(engine, completionsOptions);
+                string completion = completionsResponse.Value.Choices[0].Text;
+                Console.WriteLine($"Chatbot: {completion}");
+                recommendations.Add(completion); // Add the completion to the list
 
-            string completion = completionsResponse.Value.Choices[0].Text;
-            Console.WriteLine($"Chatbot: {completion}");
+            }
+        //    Response<Completions> completionsResponse = await client.GetCompletionsAsync(engine, completionsOptions);
 
-            recommendations.Add(completion); // Add the completion to the list
+           // string completion = completionsResponse.Value.Choices[0].Text;
+           // Console.WriteLine($"Chatbot: {completion}");
+
+           // recommendations.Add(completion); // Add the completion to the list
 
             return recommendations; // Return the list
         }
-        /*public static async Task<List<string>> GenerateOpenAIRecommendations(string text)
-        {
-            List<string> recommendations = new List<string>(); // Initialize the list
-
-            string engine = "audreysmodel";
-            OpenAIClient client = new OpenAIClient(new Uri(openAiApiendpoint), new AzureKeyCredential(openAiApiKey));
-
-            string seedText = "You are a Microsoft security engineer providing recommendations:";
-            string prompt = $"{seedText}\n{text}\nProvide security recommendations:";
-
-            // Create a completion request object
-            CompletionRequest completionRequest = new CompletionRequest
-            {
-                Engine = engine,
-                Prompt = prompt,
-                Temperature = 0.2, // Adjust this value
-                MaxTokens = 50
-            };
-
-            // Get completions asynchronously
-            Response<Completion> completionResponse = await client.Completions.CreateCompletionAsync(completionRequest);
-
-            string completion = completionResponse.Value.Choices[0].Text.Trim();
-            recommendations.Add(completion); // Add the completion to the list
-
-            return recommendations; // Return the list
-        }*/
     }
 }
